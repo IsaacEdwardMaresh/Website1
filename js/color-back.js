f26841bf6f0d650b14f23c5a57f5c5e21a4e2158
IsaacEdwardMaresh <101302919+IsaacEdwardMaresh@users.noreply.github.com>
2025-03-05 18:33:44 +0000

    Wed, Mar 5, 2025, 10:33 AM -08:00

--- js/color-back.js
+++ js/color-back.js
@@ -4,3 +4,27 @@ const element3 = document.getElementById('element3')
 const element4 = document.getElementById('element4')
 const element5 = document.getElementById('element5')
 const element6 = document.getElementById('element6')
+
+element1.addEventListener('click', ()=>{
+
+})
+
+element2.addEventListener('click', ()=>{
+
+})
+
+element3.addEventListener('click', ()=>{
+
+})
+
+element4.addEventListener('click', ()=>{
+
+})
+
+element5.addEventListener('click', ()=>{
+
+})
+
+element6.addEventListener('click', ()=>{
+
+})
