552e8f5939cdd362f9b36e8b233db097ba6a0879
IsaacEdwardMaresh <101302919+IsaacEdwardMaresh@users.noreply.github.com>
2025-03-05 18:32:03 +0000

    Wed, Mar 5, 2025, 10:32 AM -08:00

---
+++ js/color-back.js
@@ -0,0 +1,6 @@
+const element1 = document.getElementById('element1')
+const element2 = document.getElementById('element2')
+const element3 = document.getElementById('element3')
+const element4 = document.getElementById('element4')
+const element5 = document.getElementById('element5')
+const element6 = document.getElementById('element6')
