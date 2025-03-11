59b9983c37104166b8de098c7796f2801bc8c4aa
IsaacEdwardMaresh <101302919+IsaacEdwardMaresh@users.noreply.github.com>
2025-03-11 17:27:04 +0000

    Tue, Mar 11, 2025, 10:27 AM -07:00

--- js/sound.js
+++ js/sound.js
@@ -4,4 +4,19 @@ sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add ('btn')
     btn.innerHTML = sound
+btn.addEventListener('click', () => {
+    stopSongs()
+    Document.getElementById(sound).play()
 })
+
+    document.getElementById('buttons').appendChild('btn')
+})
+
+
+stopSongs(){
+    sounds.forEach(sound => {
+        document.getElementById(sound).pause(
+            document.getElementById(sound).currentTime = 0;
+        )
+    })
+}
