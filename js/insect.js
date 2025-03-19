35f77d87a87bcb40f8c3f124476fb9409102779a
IsaacEdwardMaresh <101302919+IsaacEdwardMaresh@users.noreply.github.com>
2025-03-19 16:56:00 +0000

    Wed, Mar 19, 2025, 9:56 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,11 +1,11 @@
 const screens = document.querySelectorAll('.screen')
 const start_btn = document.getElementById('start-btn')
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
-const game_container = document.getElementId('game-container')
+const game_container = document.getElementById('game-container')
 const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
 
 start_btn.addEventListener('click', () => {
-    
+    screens[0].classList.add('up')
 })
