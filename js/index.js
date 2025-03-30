fcbf416e01f2103c2b1af46071a11fab81b8ead3
IsaacEdwardMaresh <101302919+IsaacEdwardMaresh@users.noreply.github.com>
2025-03-30 00:11:21 +0000

    Sat, Mar 29, 2025, 5:11 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 const hr_hand = document.getElementById('hr_hand')
-cont button = document.querySelector(".btn")
+const button = document.querySelector(".btn")
 
 const list = ['running', 'paused']
 let index = 0
-buttony.addEventListener('click', ()=>{
+button.addEventListener('click', ()=> {
     hr_hand.style.animationPlayState = list[index]
     index = (index + 1) % 2
+}
