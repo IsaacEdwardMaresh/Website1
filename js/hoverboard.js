25da468a96837f6de13aed346afa9a8d5cd4e436
IsaacEdwardMaresh <101302919+IsaacEdwardMaresh@users.noreply.github.com>
2025-05-28 16:11:51 +0000

    Wed, May 28, 2025, 9:11 AM -07:00

--- js/hoverboard.js
+++ js/hoverboard.js
@@ -1,2 +1,8 @@
 const container = document.getElementById('container')
-const colors = ["", "", "", "", ""]
+const colors = ["#2e86c1", "#f1c40f", "#e74c3c", "#27ae60", "#f7dc6f"]
+const SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+}
