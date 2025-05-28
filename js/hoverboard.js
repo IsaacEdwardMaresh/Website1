b35a713da4481982fb7441ee1566a944cbdd0081
IsaacEdwardMaresh <101302919+IsaacEdwardMaresh@users.noreply.github.com>
2025-05-28 16:23:29 +0000

    Wed, May 28, 2025, 9:23 AM -07:00

--- js/hoverboard.js
+++ js/hoverboard.js
@@ -5,5 +5,16 @@ const SQUARES = 500
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
+    square.addEventListener('mouseover', () = > setClolor(square))
     container.appendChild(square)
+}
+
+function setColor(element){
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.box-shadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random()* colors.length)]
 }
